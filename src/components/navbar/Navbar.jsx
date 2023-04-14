376daa8c5e54e8c88ff2fe629d9c081361b3b683
lbferrand <lbferrand@gmail.com>
2023-04-14 19:31:15 -0400

    Initialize project Book Lovers app

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import "./navbar.css";
+
+const Menu = () => (
+    <>
+      <p>
+        <a href="#home">Home</a>
+      </p>
+      <p>
+        <a href="What's new">What's New</a>
+      </p>
+      <p>
+        <a href="Fiction">Fiction</a>
+      </p>
+      <p>
+        <a href="Non-Fiction">Non Fiction</a>
+      </p>
+      <p>
+        <a href="Classics">Classics</a>
+      </p>
+      <p>
+        <a href="References">References</a>
+      </p>
+      <p>
+        <a href="Contact Me">Contact Me</a>
+      </p>
+    </>
+)
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
